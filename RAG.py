d1562fdc319700c76be74177bcc69b316d96d89f
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2024-02-18 17:15:12 +0100

    trying non multi-linguar embedding model

--- RAG.py
+++ RAG.py
@@ -5,9 +5,15 @@ from LocalLLM import LocalLLM
 from langchain_community.document_loaders import PyPDFLoader
 import chromadb
 from langchain_community.llms import Ollama
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-
+# import
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_community.vectorstores import Chroma
 
 
 # initializing the embeddings
@@ -52,8 +58,6 @@ def split_doc(document, chunk_size=900, chunk_overlap=90): #
 docs = split_doc(document)
 print("---------------------------------------------------------")
 print(docs[0])
-print(docs[1])
-print(docs[2].page_content)
 print("---------------------------------------------------------")
 """
 def collect_page_contents(docs):
@@ -71,7 +75,7 @@ def clean_docs(docs):
         doc.page_content = doc.page_content.replace("\n", " ").replace("\t", " ")
     
 
-print(docs[0])
+
 clean_docs(docs)
 print("---------------------------------------------------------")
 print(docs[0])
@@ -79,10 +83,48 @@ print("---------------------------------------------------------")
 # print type of docs
 print(type(docs[0]))    
 
+def listOfAllThePageContents(docs):
+    page_contents = []
+    for doc in docs:
+        page_contents.append(doc.page_content)
+    return page_contents
+
+page_contents = listOfAllThePageContents(docs)
+
 chroma_client = chromadb.Client()
 
 docs_embeddings = embeddings.embed_documents(docs)
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+print("1")
+
+db = Chroma.from_documents(
+    docs,
+    embedding_function
+)
+print("2")
+# expose this index in a retriever interface
+retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
+
+#query = "What kind of technologies are reshaping education systems? "
+query = "Vilka teknologier omformar utbildningssystemen?"
+docs = db.similarity_search(query)
+print(docs[0].page_content)
+
+print("3")
+# create a chain to answer questions 
+qa = RetrievalQA.from_chain_type(
+    llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+print("4")
+query = "what is the total number of AI publications?"
+print("5")
+result = qa({"query": query})
+print("6")
+
+
+'''
 collection = chroma_client.create_collection(name="my_collection")
+
+#db = chromadb.Chroma.from_documents()
 def add_documents_to_collection(docs, docs_embeddings, collection):
     for i, (doc, embedding) in enumerate(zip(docs, docs_embeddings)):
         # i used for the doc_id  
@@ -94,20 +136,19 @@ def add_documents_to_collection(docs, docs_embeddings, collection):
             embeddings=[embedding.tolist()],  # convert to numpy array, then to list
             metadatas=[doc.metadata]  # metadata
         )
-        print(embedding.tolist())
 
 
 add_documents_to_collection(docs, docs_embeddings, collection)
 
 
-'''
+
 print(docs_embeddings)
 
 db = Chroma.from_documents(
     documents=docs, 
     embedding=embeddings
 )
-'''
+
 
 chain = load_qa_chain(llm, chain_type="stuff")
 
@@ -126,3 +167,4 @@ prompt = input("Ask a question about your docs: ")
 if prompt:
     answer = get_answer(prompt)
     print(f"Answer: {answer}")   
+'''
