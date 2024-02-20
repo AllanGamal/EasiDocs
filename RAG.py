c0314574bdfd5cb9397f55ead3fe040cb2d66c64
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2024-02-20 12:01:32 +0100

    Refactor document ingestion and processing

--- RAG.py
+++ RAG.py
@@ -2,98 +2,42 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
 from LocalLLM import LocalLLM
 from langchain_community.document_loaders import PyPDFLoader
-import chromadb
 from langchain_community.llms import Ollama
 from langchain_community.vectorstores import Chroma
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-from langchain_community.document_loaders import UnstructuredWordDocumentLoader
 from langchain_community.embeddings.sentence_transformer import (
     SentenceTransformerEmbeddings,
 )
 from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import TextLoader
+import document_ingestion as document_ingestion
+import embedding as embedding
 
 
 
 
 llm = Ollama(model="mistral")
 
-def load_pdf(file):
-    loader = PyPDFLoader(file)
-    documents = loader.load()
-    return documents
 
-# supports .docx, .doc, .pdf, .txt, .md
-def load_document(file):
-    if (file.endswith(".docx") | file.endswith(".doc")):
-        return UnstructuredWordDocumentLoader(file).load()
-    if (file.endswith(".pdf")):
-        return PyPDFLoader(file).load()
-    if (file.endswith(".txt") | file.endswith(".md")):
-        return TextLoader(file).load()
-    
-    return ValueError("File type not supported")
-
-def load_document_batch(files):
-    documents = []
-    for file in files:
-        documents.append(load_document(file))
-    return documents
-
-document = load_document("test.pdf")
+document = document_ingestion.load_document("test.pdf")
 #documents = load_document_batch(["test.pdf", "test.docx", "test.txt"])
 #document = load_document("test.txt")
 
-#takes the text and splits it into chunks.
-def split_document(document, chunk_size=650, chunk_overlap=150): # 
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size,
-        chunk_overlap=chunk_overlap,
-    )
-    docs = text_splitter.split_documents(document)
-    return docs
 
-def split_document_batch(documents):
-    docs = []
-    for doc in documents:
-        chunks = split_document(doc)  # returns a list of Document objects
-        for chunk in chunks:  # adds each chunk to the list of documents
-            docs.append(chunk)
-    return docs
+docs = document_ingestion.split_document(document)
+
+document_ingestion.clean_documents(docs)
 
 
-docs = split_document(document)
-
-
-def clean_documents(docs):
-    for doc in docs:
-        doc.page_content = doc.page_content.replace("\n", " ").replace("\t", " ")
-    
-
-clean_documents(docs)
-
-def get_page_contents(docs):
-    page_contents = []
-    for doc in docs:
-        page_contents.append(doc.page_content)
-    return page_contents
-
-page_contents = get_page_contents(docs)
-
-chroma_client = chromadb.Client()
+page_contents = document_ingestion.get_page_contents(docs)
 
 embedding_function = SentenceTransformerEmbeddings(model_name="intfloat/multilingual-e5-large")
 
 
-def init_database(docs, embedding_function):
-    db = Chroma.from_documents(
+db = Chroma.from_documents(
         docs,
         embedding_function,
     )
-    return db
-
-db = init_database(docs, embedding_function)
 
 
 def search_database(db, query, k=3):
--- localLLM.py
+++ localLLM.py
@@ -1,13 +1,15 @@
 import openai
 from langchain.llms import BaseLLM
 
+
+# If using LLM through the LMstudo 
 class LocalLLM(BaseLLM):
     def __init__(self, base_url="http://localhost:1234/v1"):
         super().__init__()
         self.client = openai.OpenAI(base_url=base_url, api_key="not-needed")
 
     def _generate(self, prompt, **kwargs):
-        # Denna metod bör anropa OpenAI's API och returnera svaret
+        
         try:
             completion = self.client.chat.completions.create(
                 model="local-model",
@@ -23,10 +25,9 @@ class LocalLLM(BaseLLM):
             return None
 
     def _llm_type(self):
-        # Denna metod bör returnera en sträng som representerar typen av LLM. Detta är ett exempel:
+        
         return "LocalLLM"
 
-    # 'run'-metoden kan behållas om den behövs för andra syften,
-    # men '_generate'-metoden är den som används av 'BaseLLM' för att generera text.
+    
     def run(self, prompt, **kwargs):
         return self._generate(prompt, **kwargs)
