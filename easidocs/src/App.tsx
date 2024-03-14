e5556a01674eccda5a80c107ba8950db725075be
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2024-03-14 19:24:35 +0100

    initial layout made in tauri

--- easidocs/src/App.tsx
+++ easidocs/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
@@ -13,39 +14,80 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="main-container">
+      <div className="file-container">
+        <h1 id="file-title">Files</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul className="list-group file-list">
+          <li className="list-group-item file-item">
+            <label>File 1</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+          <li className="list-group-item file-item">
+            <label>File 2</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+          <li className="list-group-item file-item">
+            <label>File 3</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+          <li className="list-group-item file-item">
+            <label>File 4</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+          <li className="list-group-item file-item">
+            <label>File 5</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+          <li className="list-group-item file-item">
+            <label>File 6</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+          <li className="list-group-item file-item">
+            <label>File 7</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+          <li className="list-group-item file-item">
+            <label>File 8</label>
+            <button className="btn-close remove-file-button"></button>
+          </li>
+        </ul>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+      <div className="chat-container">
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
 
-      <p>{greetMsg}</p>
+        <ul className="chat-history-container list-group">
+
+          <li className="chat-user-container list-group-item">
+            <div className="chat-user-logo">
+              <img src={reactLogo} className="App-logo" alt="logo" />
+            </div>
+
+            <div className="chat-user">
+              <p className="chat-history-textfield">This is a test question from the user</p>
+            </div>
+          </li>
+
+          <li className="chat-user-container list-group-item">
+            <div className="chat-user-logo">
+              <img src={reactLogo} className="App-logo" alt="logo" />
+            </div>
+            <div className="chat-bot">
+              <p className="chat-history-textfield">This is a test answer from the bot</p>
+            </div>
+          </li>
+
+        </ul>
+
+        <div className="chat-input-container">
+          <input type="text" className="chat-input" placeholder="Ask a question..."></input>
+          <button type="button" className="btn btn-primary">Send</button>
+        </div>
+      </div>
+
+
+
     </div>
   );
 }
