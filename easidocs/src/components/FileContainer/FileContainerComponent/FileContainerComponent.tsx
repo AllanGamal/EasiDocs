791649c10c5b3cf7d8d7307435f3137efcab32a8
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2024-03-18 14:40:56 +0100

    uploading animation done

--- easidocs/src/components/FileContainer/FileContainerComponent/FileContainerComponent.tsx
+++ easidocs/src/components/FileContainer/FileContainerComponent/FileContainerComponent.tsx
@@ -8,14 +8,17 @@ import axios from 'axios';
 function FileContainerComponent() {
   const [dragging, setDragging] = useState(false);
   const [fileNames, setFileNames] = useState<string[]>([]);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
 
   const onRemoveFile = (file: string) => {
     const apiUrl = "http://localhost:8001/delete";
-    
-    
+
+
     axios.delete(apiUrl, { data: { file_path: "pdf/" + file } })
-    .then(response => {
-      if (response.status === 200) {
+      .then(response => {
+        if (response.status === 200) {
           setFileNames(existingFileNames => existingFileNames.filter(name => name !== file));
           console.log('File deleted');
         } else {
@@ -38,17 +41,18 @@ function FileContainerComponent() {
     setDragging(false);
   };
 
-  
+
 
   const handleFileUpload = (file_paths: string[]) => {
-    
+
     const apiUrl = 'http://localhost:8001/upload';
     console.log("file_paths");
     console.log(file_paths);
-    
+    setIsUploading(true);
     // send the array of paths to the server
     axios.post(apiUrl, { file_paths })
       .then(response => {
+        setIsUploading(false);
         if (response.status === 200) {
           console.log(response.data);
           console.log('Files uploaded');
@@ -57,6 +61,7 @@ function FileContainerComponent() {
         }
       })
       .catch(error => {
+        setIsUploading(false);
         console.error(error);
       });
   }
@@ -77,42 +82,52 @@ function FileContainerComponent() {
   useEffect(() => {
     loadFileList();
   }
-  , []);
+    , []);
 
   useEffect(() => {
-      const unlisten = listen('tauri://file-drop', event => {
-        
-        const filePaths = event.payload; 
-        if (Array.isArray(filePaths)) {
-          
-          const validFileTypes = ['pdf', 'docx', 'doc', 'txt', 'md']; // remove files from filePaths that is not pdf, docx, doc, txt, md files
-          const validFilePaths = filePaths.filter(path => validFileTypes.includes(path.split('.').pop() || ''));
-          const newFileNames = validFilePaths.map(path => path.split('/').pop() || '');
+    const unlisten = listen('tauri://file-drop', event => {
 
-          // Filter out file names that already exist
-          const uniqueFileNames = newFileNames.filter(name => !fileNames.includes(name));
-          setFileNames(existingFileNames => [...existingFileNames, ...uniqueFileNames]);
+      const filePaths = event.payload;
+      if (Array.isArray(filePaths)) {
 
-          // Only upload files that are not already in the list
-          const uniqueFilePaths = validFilePaths.filter((index) => !fileNames.includes(newFileNames[index]));
-          handleFileUpload(uniqueFilePaths);
-          
-        }
+        const validFileTypes = ['pdf', 'docx', 'doc', 'txt', 'md']; // remove files from filePaths that is not pdf, docx, doc, txt, md files
+        const validFilePaths = filePaths.filter(path => validFileTypes.includes(path.split('.').pop() || ''));
+        const newFileNames = validFilePaths.map(path => path.split('/').pop() || '');
 
-        setDragging(false);
-      });
+        // Filter out file names that already exist
+        const uniqueFileNames = newFileNames.filter(name => !fileNames.includes(name));
+        setFileNames(existingFileNames => [...existingFileNames, ...uniqueFileNames]);
 
-      return () => {
-        unlisten.then((fn) => fn());
-      };
-    }, [fileNames]); // Add fileNames as a dependency
+        // Only upload files that are not already in the list
+        const uniqueFilePaths = validFilePaths.filter((index) => !fileNames.includes(newFileNames[index]));
+        handleFileUpload(uniqueFilePaths);
+
+      }
+
+      setDragging(false);
+    });
+
+    return () => {
+      unlisten.then((fn) => fn());
+    };
+  }, [fileNames]); // Add fileNames as a dependency
 
   return (
     <div
-      className="file-container"
+      className={`file-container ${isUploading ? 'uploading' : ''}`}
     >
       <h1 id="file-title">easiDocs</h1>
-      <FileListComponent files={fileNames} onRemoveFile={onRemoveFile} />
+      {isUploading && (
+        <div className="uploading-text" style={{ color: "white" }}>
+          <label>Uploading</label>
+          <label> Please wait...</label>
+          </div>
+      )}
+
+      <div
+        className={`file-container-zone ${isUploading ? 'uploading' : ''}`}
+      >
+        <FileListComponent files={fileNames} onRemoveFile={onRemoveFile} />
         <div
           className={`dropZone ${dragging ? 'dragging' : ''}`}
           onDragOver={(e) => dragOver(e)}
@@ -120,6 +135,7 @@ function FileContainerComponent() {
           DROP FILES
         </div>
       </div>
+    </div>
 
   );
 }
